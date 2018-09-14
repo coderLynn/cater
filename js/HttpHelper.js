e6bef415407a6a059213c6fffecee7593bf6ab13
ylz <653816757@qq.com>
2018-09-14 17:09:55 +0800

    123

---
+++ js/HttpHelper.js
@@ -0,0 +1,151 @@
+﻿var host = "http://chifenapi.endingtech.com";
+/**
+ * POST      
+ * @param {string} url
+ * @param {JSON} data
+ * @param {function} callback
+ * @param {function} complete
+ */
+var post = function (url, data, callback, complete) {
+    var url_full = geturl(url);
+    ajax("POST", 1, url_full, data, callback, complete);
+}
+/**
+ * POST JSON OBJECT
+ * @param {any} url
+ * @param {any} data
+ * @param {any} callback
+ * @param {any} complete
+ */
+var postJson = function (url, data, callback, complete) {
+    var url_full = geturl(url);
+    ajax("POST", 0, url_full, data, callback, complete);
+}
+/**
+ * GET
+ * @param {string} url
+ * @param {JSON} data
+ * @param {function} callback
+ * @param {function} complete
+ */
+var get = function (url, data, callback, complete) {
+    var url_full = geturl(url);
+    ajax("GET", 1, url_full, data, callback, complete);
+}
+/**
+ * ajax
+ * @param {any} method
+ * @param {any} url
+ * @param {any} data
+ * @param {any} callback
+ * @param {any} complete
+ */
+var ajax = function (method, dataType, url, data, callback, complete) {
+    $.ajax({
+        type: method,
+        url: url,
+        data: dataType == 0 ? JSON.stringify(data) : data,
+        contentType: dataType == 0 ? "application/json" : "application/x-www-form-urlencoded",
+        dataType: "json",
+
+        complete: function (msg) {
+            if (msg.status != 200) {
+                if (typeof (complete) == "function") {
+                    complete(msg);
+                }
+                alert("系统错误", "error");
+            }
+        },
+        success: function (msg) {
+            if (typeof (complete) == "function") {
+                complete(msg);
+                console.log(1)
+            }
+            if (msg.code != 0) {
+                if (msg.code == 2) {
+                    alertDo(msg.msg, function () {
+                        location.href = "/login";
+                    }, "error", "登录失效");
+                } else {
+                    alert(msg.err, "error");
+                }
+            } else {
+                if (typeof (callback) == "function") {
+                    callback(msg.data);
+                }
+            }
+        }
+    })
+}
+/**
+ * 文件上传
+ * @param {any} url
+ * @param {any} data
+ * @param {any} success
+ * @param {any} onprogress
+ * @param {any} index 索引
+ */
+var postfile = function (url, data, success, onprogress,index) {
+    var dataString = new FormData();
+    for (var p in data) {
+        dataString.append(p, data[p]);
+    }
+    $.ajax({
+        url: url,
+        type: 'POST',
+        xhr: function () {  //  XMLHttpRequest
+            var myXhr = $.ajaxSettings.xhr();
+            if (myXhr.upload) { // 这里能做好多事
+                myXhr.upload.onprogress = function (e) {
+                    //进度自己再这里写
+                    var percent = e.loaded / e.total;//计算百分比
+                    if (typeof (onprogress) == "function") {
+                        onprogress(percent, index);
+                    }
+                }
+            }
+            return myXhr;
+        },
+        success: function (msg) {
+            if (msg.code != 0) {
+                alert(msg.err, "error");
+            } else {
+                if (typeof (success) == "function") {
+                    success(msg.data);
+                }
+            }
+        },
+        error: function (r) {
+            alert("上传出错,请重新打开页面继续", "error");
+        },
+        data: dataString,
+        //关键是这个东西
+        enctype: "multipart/form-data",
+        cache: false,
+        contentType: false,
+        processData: false
+    });
+}
+var geturl = function (url) {
+    var url_full = host + url;
+
+    return url_full;
+}
+
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (elt /*, from*/) {
+        var len = this.length >>> 0;
+        var from = Number(arguments[1]) || 0;
+        from = (from < 0)
+            ? Math.ceil(from)
+            : Math.floor(from);
+        if (from < 0)
+            from += len;
+        for (; from < len; from++) {
+            if (from in this &&
+                this[from] === elt)
+                return from;
+        }
+        return -1;
+    };
+}
---
+++ js/WeChatJSSDK.js
@@ -0,0 +1,31 @@
+﻿Wechat = function (doWechat) {
+    get("/api/WeChat/GetJsSdkInfo", { url: location.href.split('#')[0] }, function (json) {
+        wx.config({
+            debug: false,//开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            appId: json.appId,// 必填，公众号的唯一标识
+            timestamp: json.timestamp,// 必填，生成签名的时间戳
+            nonceStr: json.nonceStr,// 必填，生成签名的随机串
+            signature: json.signature,// 必填，签名
+            jsApiList: [
+                'checkJsApi',
+                'onMenuShareTimeline',
+                'onMenuShareAppMessage',
+                'onMenuShareQQ',
+                'onMenuShareWeibo',
+                'closeWindow',
+                'scanQRCode',
+                'openLocation',
+                'getLocation',
+                'chooseWXPay'
+            ]// 必填，需要使用的JS接口列表
+        });
+        wx.ready(function () {
+            if (typeof (doWechat) == "function") {
+                doWechat(wx);
+            }
+        });
+        wx.error(function (res) {
+            alert(res.errMsg);
+        });
+    });
+}
